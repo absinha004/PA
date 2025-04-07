921ad966dd6df60862cb02500e7b34a8413f7f60
Sinha-Abhinav-13 <absinha004@gmail.com>
2025-04-07 14:54:22 +0530

    posthog deployment

--- test-your-skills/src/index.js
+++ test-your-skills/src/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom/client'; // Ensure correct import
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { PostHogProvider } from 'posthog-js/react';
 
 const rootElement = document.getElementById('root');
-
 if (!rootElement) {
-    throw new Error("Root element not found!");
+  throw new Error("Root element not found!");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+};
 
+const root = ReactDOM.createRoot(rootElement);
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
+  </React.StrictMode>
 );
